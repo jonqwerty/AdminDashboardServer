51ca2c448127bb3a0ee7219c58b94036fbbe50a0
Serhii B <s_blyshchak@ukr.net>
2023-02-14 23:34:08 +0200

    Implrment overview page server

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ import User from "./models/User.js"
 import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
 import Transaction from "./models/Transaction.js"
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import OverallStat from "./models/OverallStat.js"
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+} from "./data/index.js"
 
 dotenv.config()
 const app = express()
@@ -42,6 +49,7 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
+    // OverallStat.insertMany(dataOverallStat)
     // Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
     // Transaction.insertMany(dataTransaction)
